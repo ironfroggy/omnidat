a4239bb5b2e9a973a69f1eacbbb4d95da3000ce1
Calvin Spealman <ironfroggy@gmail.com>
2012-09-10 21:23:05 -0400

    Moved omnidat to a package

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "omnidat",
+    version = "0.1-pre-1",
+    description = "A loose data tool",
+    author = "Calvin Spealman",
+    author_email='ironfroggy@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
